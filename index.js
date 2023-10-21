7594e1262cd35ea34e45446e45c24ebf5e64c919
PoomrapeeS <poomrapee.s@outlook.co.th>
2023-10-21 19:21:50 +0700

    First design of the project

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import express from "express";
 const app = express();
 const port = 3000;
@@ -5,8 +6,17 @@ const port = 3000;
 // Serve static files from the public folder
 app.use(express.static('public'));
 
+const apiUrl = "https://api.jikan.moe/v4/random/anime";
+
 app.get('/', async(req, res) => {
-    res.render("index.ejs");
+  try{
+    const response = await axios.get(apiUrl);
+    const data = response.data.data;
+    // const img = data.images.jpg.large_image_url;
+    res.render("index.ejs", {data: data});
+  } catch(error){
+    console.log(error)
+  }
 })
 
 app.listen(port, () => {
